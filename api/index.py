f729ae8b070a06f80bac4465dab63ded96db42ad
Soumyadeep Das <133207548+Soumyadeep765@users.noreply.github.com>
2024-07-29 18:43:35 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query, Request
+from fastapi import FastAPI, Query, HTTPException, Request
 from pydantic import BaseModel
 import requests
 import random
@@ -9,21 +9,22 @@ app = FastAPI()
 class UsernameRequest(BaseModel):
     username: str = None
 
-def generate_random_username(length=8):
-    """Generate a random username with diverse characters."""
-    characters = string.ascii_letters + string.digits + '_'
-    return ''.join(random.choices(characters, k=length))
+def generate_random_username(length=6):
+    """Generate a random username."""
+    return ''.join(random.choices(string.ascii_lowercase, k=length))
 
 def check_username_validity(username):
-    """Check if the provided username is available."""
+    """Check the validity of the provided username."""
     url = f"https://t.me/{username}"
     response = requests.get(url)
     if response.status_code == 200:
         response_text = response.text
-        if "<div class=\"tgme_page_description\">" in response_text:
+        if "</div>\n<div class=\"tgme_page_description\">\n  If you have <strong>Telegram</strong>, you can contact <a class=\"tgme_username_link\" href=\"tg:resolve?domain=" in response_text:
             return False
-        else:
+        elif f"<div class=\"tgme_page_extra\">\n  @{username}\n</div>" in response_text or f"<div class=\"tgme_page_context_link_wrap\"><a class=\"tgme_page_context_link\" href=\"/s/{username}\">Preview channel</a></div>" in response_text:
             return True
+        else:
+            return False
     else:
         return False
 
